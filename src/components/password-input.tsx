2e7e2f72288c12e1603268c1845b024d860c8cac
supuwoerc <zhangzhouou@gmail.com>
2026-02-27 20:52:32 +0800

    fix: fix password input form submission and update translations

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -12,12 +12,13 @@ type PasswordInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & 
   disabled?: boolean
 }
 
-const PasswordInput: FC<PasswordInputProps> = ({ className, disabled, ref }) => {
+const PasswordInput: FC<PasswordInputProps> = ({ className, disabled, ref, ...props }) => {
   const [showPassword, setShowPassword] = useState(false)
   return (
     <div className={cn('rounded-2md relative', className)}>
       <input
         ref={ref}
+        {...props} // Bind additional properties, such as properties injected by react-hook-form.
         type={showPassword ? 'text' : 'password'}
         autoComplete="off"
         className="h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40"
@@ -27,6 +28,7 @@ const PasswordInput: FC<PasswordInputProps> = ({ className, disabled, ref }) => 
         size="icon"
         variant="ghost"
         disabled={disabled}
+        type="button" // Prevent the form from being submitted by default.
         onClick={() => setShowPassword((prev) => !prev)}
       >
         {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
