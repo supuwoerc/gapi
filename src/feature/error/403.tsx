55d058685491dd5198bb7c2475dad45c17ea2efd
supuwoerc <zhangzhouou@gmail.com>
2026-03-02 20:07:17 +0800

    feat: add 403 forbidden page with i18n support and assets

---
+++ src/feature/error/403.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router'
+
+import forbidden from '@/assets/error/forbidden.png'
+
+import { Button } from '@/components/ui/button'
+
+const Forbidden = () => {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+  return (
+    <div className="h-svh w-svw">
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <img src={forbidden} alt="not-fount" className="w-32" />
+        <span className="font-medium">{t('error.forbidden.title')}</span>
+        <p className="text-center text-muted-foreground">{t('error.forbidden.subtitle')}</p>
+        <div className="mt-6 flex gap-4">
+          <Button variant="outline" onClick={() => navigate(-1)}>
+            {t('common.button.goBack')}
+          </Button>
+          <Button onClick={() => navigate('/')}>{t('common.button.backToHome')}</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Forbidden
