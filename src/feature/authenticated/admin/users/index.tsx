69f726d337cfa4634b052f88134efe94186f4714
supuwoerc <zhangzhouou@gmail.com>
2026-03-13 20:43:39 +0800

    feat: add page header with navigation toolbar and layout wrapper

--- src/feature/authenticated/admin/users/index.tsx
+++ src/feature/authenticated/admin/users/index.tsx
@@ -1,8 +1,15 @@
 import { useDataTable } from '@/hooks/use-data-table'
 
+import { ConfigDrawer } from '@/components/config-drawer'
 import { DataTable } from '@/components/data-table/data-table'
 import { DataTableSortList } from '@/components/data-table/data-table-sort-list'
 import { DataTableToolbar } from '@/components/data-table/data-table-toolbar'
+import { LanguageSwitcher } from '@/components/language-switcher'
+import AppHeader from '@/components/layout/authenticated/app-header'
+import AppMain from '@/components/layout/authenticated/app-main'
+import { ProfileDropdown } from '@/components/profile-dropdown'
+import Search from '@/components/search'
+import { ThemeModeSwitcher } from '@/components/theme-mode-switcher'
 
 import { columns } from './data/columns'
 import { users } from './data/data'
@@ -15,22 +22,30 @@ const Users = () => {
   })
 
   return (
-    <div className="flex flex-1 flex-col gap-6 p-6">
-      <div className="flex items-center justify-between">
-        <div>
-          <h1 className="text-2xl font-bold tracking-tight">Users</h1>
-          <p className="text-muted-foreground">
-            Manage your team members and their account permissions.
-          </p>
+    <>
+      <AppHeader fixed>
+        <Search />
+        <div className="ms-auto flex items-center space-x-4">
+          <ThemeModeSwitcher />
+          <LanguageSwitcher />
+          <ConfigDrawer />
+          <ProfileDropdown />
         </div>
-      </div>
-
-      <DataTable table={table}>
-        <DataTableToolbar table={table}>
-          <DataTableSortList table={table} />
-        </DataTableToolbar>
-      </DataTable>
-    </div>
+      </AppHeader>
+      <AppMain className="flex flex-col gap-4 sm:gap-6">
+        <div className="flex flex-wrap items-end justify-between gap-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">User List</h2>
+            <p className="text-muted-foreground">Manage your users and their permissions here.</p>
+          </div>
+        </div>
+        <DataTable table={table}>
+          <DataTableToolbar table={table}>
+            <DataTableSortList table={table} />
+          </DataTableToolbar>
+        </DataTable>
+      </AppMain>
+    </>
   )
 }
 
